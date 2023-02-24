df1bedec5727971205944ae19e3d3decaeffe041
Zolo <zolo@unicornab.se>
2023-02-24 12:32:29 +0100

    cleanup ex12

--- exercise12/ex12.c
+++ exercise12/ex12.c
@@ -4,21 +4,20 @@
  * Author        : zolodev
  * Github        : https://github.com/zolodev
  * Description   : Sizes and Arrays
-*****************************************************************************/
+ *****************************************************************************/
 
 #include <stdio.h>
 
 int main(int argc, char *argv[])
 {
-    int areas[] = { 10, 12 , 13, 14, 20 };
+    int areas[] = {10, 12, 13, 14, 20};
     char name[] = "Zolo";
     char full_name[] = {
         'Z', 'o', 'l', 'o',
         ' ', 'Z', '.', ' ',
-        'D', 'e', 'v', '\0'
-    };
+        'D', 'e', 'v', '\0'};
 
-    // WARNING: On some systems you may have to change the 
+    // WARNING: On some systems you may have to change the
     // %ld in this code to a %u since it will use unsigned ints
     printf("The size of an int: %ld\n", sizeof(int));
     printf("The size of areas (int[]): %ld\n", sizeof(areas));
@@ -36,5 +35,4 @@ int main(int argc, char *argv[])
     printf("name=\"%s\" and full_name=\"%s\"\n", name, full_name);
 
     return 0;
-
 }
