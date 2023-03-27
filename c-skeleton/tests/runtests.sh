75fe7e40e8f57d8aceb87df3938b6a24dab02ff9
Zolo <zolo@unicornab.se>
2023-03-27 10:43:06 +0200

    Updating runtests script

--- c-skeleton/tests/runtests.sh
+++ c-skeleton/tests/runtests.sh
@@ -1,2 +1,22 @@
 # TODO: script to run tests
-echo "Faking running tests..."
+echo "Running unit tests:"
+
+LOG_FILE=tests/tests.log
+
+for i in tests/*_tests
+do
+    if test -f $i
+    then
+        if $VALGRIND ./$i 2>> $LOG_FILE
+        then
+            echo $i PASS
+        else
+            echo "ERROR in test $i: here's $LOG_FILE"
+            echo "-----"
+            tail $LOG_FILE
+            exit 1
+        fi
+    fi
+done
+
+echo ""
