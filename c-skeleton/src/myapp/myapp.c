d572a014fe7ea17c52af97016a19334cc1364d48
Zolo <zolo@unicornab.se>
2023-03-28 10:39:58 +0200

    Updating skeleton

---
+++ c-skeleton/src/myapp/myapp.c
@@ -0,0 +1,14 @@
+/*****************************************************************************
+ * Filename      : myapp.c
+ * Created       : Tue Mar 28 2023
+ * Author        : Zolo
+ * Github        : https://github.com/zolodev
+ * Description   :
+ *****************************************************************************/
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello world!\n");
+  return 0;
+}
