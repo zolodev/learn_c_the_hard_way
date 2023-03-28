27533a37ed1d82162fa03303e2a9ab5653e4358e
The Jedi <zolodev@jonzzon.nu>
2023-03-28 10:08:41 +0200

    Updating exercise29

--- exercise29/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Build Exercise29
-cc -c libex29.c -o build/libex29.o
-cc -shared -o build/libex29.so build/libex29.o
-cc -Wall -g -DNDEBUG ex29.c -ldl -o build/ex29
--- exercise29/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-./build/ex29 ./build/libex29.so print_message "This is a test string to print"
