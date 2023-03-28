db994ba1ae4bdf6943ed76ed5eae1f7b6085295d
Zolo <zolo@unicornab.se>
2023-03-28 13:21:56 +0200

    updating dbg.h in root

--- dbg.h
+++ dbg.h
@@ -4,6 +4,8 @@
  * Author        : Zolo
  * Github        : https://github.com/zolodev
  * Description   : Zed A. Shaws Awesome Debug Macros [modded]
+ * For more information about colors and more see:
+ * https://stackoverflow.com/questions/4842424/list-of-ansi-color-escape-sequences
  *****************************************************************************/
 
 // Toggle Disable DEBUG messages
@@ -18,11 +20,25 @@
 #include <stdio.h>
 #include <string.h>
 
+// #define COLORS
+#ifdef COLORS
 #define C_RED "\x1b[31m"
 #define C_BLUE "\x1b[34m"
-#define C_CYAN "\x1b[44m"
+#define C_CYAN "\x1b[36m"
+#define C_GREEN_BOLD "\x1b[32;1m"
 #define C_YELLOW "\x1b[93m"
+#define C_MAGENTA "\x1b[35m"
 #define C_RESET "\x1b[0m"
+#else
+#define C_RED ""
+#define C_BLUE ""
+#define C_CYAN ""
+#define C_GREEN_BOLD ""
+#define C_YELLOW ""
+#define C_MAGENTA ""
+#define C_RESET ""
+
+#endif
 
 // compile with the CPPFLAGS=-DNDEBUG to set/activate NO DEBUG = "FALSE"
 // Using debug() in the code will not be printed unless the flag is removed.
@@ -31,16 +47,16 @@
 #ifdef NDEBUG
 #define debug(M, ...)
 #else
-#define debug(M, ...)                                                        \
-  fprintf(stderr, C_CYAN "DEBUG %s:%d: " M "\n" C_RESET, __FILE__, __LINE__, \
-          ##__VA_ARGS__)
+#define debug(M, ...)                                                   \
+  fprintf(stderr, C_MAGENTA "[DEBUG](%s:%d) " M "\n" C_RESET, __FILE__, \
+          __LINE__, ##__VA_ARGS__)
 #endif
 
 #define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-#define log_err(M, ...)                                                    \
-  fprintf(stderr, C_RED "[ERROR] (%s:%d:%s(): errno: %s) " M "\n" C_RESET, \
-          __FILE__, __LINE__, __FUNCTION__, clean_errno(), ##__VA_ARGS__)
+#define log_err(M, ...)                                                        \
+  fprintf(stderr, C_RED "[ERROR](%s:%d, errno: %s) " M "\n" C_RESET, __FILE__, \
+          __LINE__, clean_errno(), ##__VA_ARGS__)
 
 #define log_warn(M, ...)                                                \
   fprintf(stderr, C_YELLOW "[WARN] (%s:%d: errno: %s) " M "\n" C_RESET, \
@@ -49,6 +65,10 @@
 #define log_info(M, ...)                                                       \
   fprintf(stderr, C_BLUE "[INFO] (%s:%d) " M "\n" C_RESET, __FILE__, __LINE__, \
           ##__VA_ARGS__)
+
+#define log_info_green_bold(M, ...)                                        \
+  fprintf(stderr, C_GREEN_BOLD "[INFO] (%s:%d) " M "\n" C_RESET, __FILE__, \
+          __LINE__, ##__VA_ARGS__)
 
 #define check(A, M, ...)       \
   if (!(A)) {                  \
